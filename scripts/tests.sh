6965278380dd5ddddfb15511a051a7b7e1aada15
Caio <c410.f3r@gmail.com>
2021-02-22 22:52:02 -0300

    Update dependencies

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -6,6 +6,7 @@ cargo install --git https://github.com/c410-f3r/rust-tools --force
 
 rt='rust-tools --template you-rust'
 
+export CARGO_TARGET_DIR="$($rt target-dir)"
 export RUST_BACKTRACE=1
 export RUSTFLAGS="$($rt rust-flags '' -Dmissing_docs)"
 
